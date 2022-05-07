bc55ad40d47a52e754a5da310b0a09eca41746ba
Luka Licheli <lukalicheli@gmail.com>
2022-05-07 01:45:25 -0700

    Solved log-nums algorithm

--- 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,25 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    arr = [];
+    while(num >= 1) {
+       arr.unshift(num);
+       num = num -1;
+
+//unneccessary code
+       
+
+    
+
+        
+
+    }
+    for(i=1; i<= arr.length; i++){
+        console.log(i);
+    }
+    
+};
+
+
+// first when number comes in push that to Array, then decrease by 1 name that a method and 
