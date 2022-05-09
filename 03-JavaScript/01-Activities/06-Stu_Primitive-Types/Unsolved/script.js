9fa94627a989098d749a5da5110d3910e10fed30
Luka Licheli <lukalicheli@gmail.com>
2022-05-08 17:58:05 -0700

    Finished activity 6 unit 3

--- 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -6,18 +6,18 @@ var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof one); 
-console.log(typeof two); 
-console.log(typeof three); 
-console.log(typeof four); 
-console.log(typeof five);
-console.log(typeof six);
+console.log(typeof one); //number
+console.log(typeof two);  // string
+console.log(typeof three); // boolean
+console.log(typeof four); // number
+console.log(typeof five); // String
+console.log(typeof six); // Undefined
 
 four = "Hello!";
 five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof four);
-console.log(typeof five);
-console.log(typeof six);
+console.log(typeof four); // String
+console.log(typeof five); // Boolean
+console.log(typeof six); // Number
