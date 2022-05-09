093f7acf0da8d49a99047fe2a74cb220a175a9e6
Luka Licheli <lukalicheli@gmail.com>
2022-05-08 22:43:23 -0700

    Finished activity 10 unit 3

--- 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -4,3 +4,17 @@ var expression1 = (x < 25);
 var expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
+
+if(expression1 && expression2) {
+    console.log("True, True");
+}
+
+if (expression1 || !expression2) {
+    console.log(" True, False");
+}
+if (!expression1 || expression2) {
+    console.log(" False, True");
+}
+if (!expression1 && !expression2) {
+    console.log(" False, False");
+}
