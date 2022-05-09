e6c96f306f31368e34c48079528937a6a6ecd08a
Luka Licheli <lukalicheli@gmail.com>
2022-05-09 00:25:43 -0700

    Working on activity 16 unit 3

--- 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
+++ 03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
@@ -2,6 +2,16 @@
 
 // TODO: Create a function that accepts two parameters and prints the correct
 // message according to the acceptance criteria.
+let message = function(param1, param2) { 
+    if (param1.typeOf === param2.typeOf && param1.value === param2.value) {
+        console.log("They are equal in type and value");
+    }
+    
+}
+
+message(1, "fasdf");
+
+
 
 // TODO: call the function with two arguments that are equal in type and value.
 
