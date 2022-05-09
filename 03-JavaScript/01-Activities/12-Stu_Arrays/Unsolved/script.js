8010bd572bb2a6bd60d42eccc2adeaddfe306eb8
Luka Licheli <lukalicheli@gmail.com>
2022-05-09 00:02:29 -0700

    Activity 12 unit 3 is complete

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,18 @@
 // WRITE YOUR CODE HERE
+let studentArr = ["Luka", "Shekib", "Jamshid"];
+
+let numberOfStudents = studentArr.length; 
+
+console.log(numberOfStudents);
+
+for(i=0; i < numberOfStudents; i++ ) {
+    console.log(studentArr[i]);
+}
+
+studentArr[0] = "Stephanie";
+
+console.log(studentArr);
+
+replacedName = studentArr[0];
+
+console.log(replacedName + " is in class");
