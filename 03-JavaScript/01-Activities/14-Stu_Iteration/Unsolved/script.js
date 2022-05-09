4d8531579689ba4d70fc69e6cc295b3f390f6577
Luka Licheli <lukalicheli@gmail.com>
2022-05-09 00:13:43 -0700

    Completed activity 14 unit 3

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,15 @@
 //WRITE YOUR CODE BELOW
+let students = [
+ "Luka",
+ "Shekib", 
+ "Jamshid", 
+ "Stephanie"
+]
+
+for(let i=0; i < students.length; i++ ) {
+    console.log(
+        "Great to see you,", 
+        students[i], 
+        "!"
+    )
+}
