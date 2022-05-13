f0c94815f52710ee9142e73eeb05b3cbb19ea126
Luka Licheli <lukalicheli@gmail.com>
2022-05-12 23:47:45 -0700

    Solved activity 22 unit 3

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,7 +1,17 @@
 // TODO: create customerDrink object with name, sugars, and isReady properties
-
+let customerDrink = {
+    name: "Luka",
+    sugars: 3, 
+    isReady: true,
+}
 // TODO: print the customerDrink object
-
+console.log(customerDrink);
 // TODO: print ready message IF the drink is ready
+if(customerDrink.isReady) {
+    console.log("Ready for pick-up");
+}
 
 // TODO: print in queue message IF the drink is not ready
+if(!customerDrink.isReady) {
+    console.log("in queue");
+}
