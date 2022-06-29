cf26f26119b207c67db59bfe3f2b1da503be4391
Luka Licheli <lukalicheli@gmail.com>
2022-06-28 18:02:29 -0700

    Activity 2 javascript

--- 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,20 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(n) {
+    function fizzBuzz(n) {
+    
+        // Write your code here
+        for(let i=0; i<=n; i++) {
+            if(i%15 ===0) {
+                console.log("FizzBuzz");
+            } else if(i%5 === 0) {
+                console.log("Buzz");
+            } else if (i%3 ===0) {
+                console.log("Fizz");
+            } else {
+                return i;
+            }
+        }
+    }
+};
